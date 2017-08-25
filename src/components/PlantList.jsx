dd0549cab07b2dac9923d7c3cd81f53c004245d1
Jared Eiseman <jathei@gmail.com>
2017-08-25 14:27:03 -0700

    add sort to PlantList to show plants that were watered least recently at top of list

--- src/components/PlantList.jsx
+++ src/components/PlantList.jsx
@@ -2,8 +2,21 @@ import React from 'react';
 import Plant from './Plant';
 import styles from '../styles/App.css';
 import PropTypes from 'prop-types';
+import Moment from 'moment';
 
 function PlantList(props) {
+  var toDisplay = props.childPlantList.sort((a,b) => {
+    var first = new Moment(a.lastWatering);
+    var second = new Moment(b.lastWatering);
+    if (first.isBefore(second)) {
+      return -1;
+    } else if (first.isAfter(second)) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+
   return (
     <div className={styles.plantList}>
       {props.childPlantList.map((plant, index) =>
